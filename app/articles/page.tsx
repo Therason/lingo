89d816cd7ae32a956841dde469c9f0d0c2ba658d
Thomas Broadwater <tgbroadh2o@gmail.com>
2023-08-28 10:47:37 -0400

    Initial commit

---
+++ app/articles/page.tsx
@@ -0,0 +1,11 @@
+import styles from './articles.module.css'
+
+export default function Articles() {
+  return (
+    <main className={styles.main}>
+      <header>
+        <h2>Noun article practice</h2>
+      </header>
+    </main>
+  )
+}
---
+++ components/card/index.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+function Card() {
+  return (
+    <>
+      <h3>some info about card idk</h3>
+    </>
+  )
+}
+
+export { Card }
