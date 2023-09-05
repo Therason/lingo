e982d1884003c4044e4950a1c259bc4c5a4f7313
Thomas Broadwater <tgbroadh2o@gmail.com>
2023-09-05 09:46:22 -0400

    Basic sentence type querying

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -2,20 +2,33 @@ import styles from './articles.module.css'
 import ArticlesClient from './articlesClient'
 import prisma from '@/lib/db'
 
-async function getSentences() {
+async function getSentences(query?: {
+  [key: string]: string | string[] | undefined
+}) {
+  // parse params
+  let isDifficult = false
+  if (query?.difficult === 'true') isDifficult = true
+
   const sentences = await prisma.sentence.findMany({
-    take: 5,
+    take: 3,
+    where: {
+      difficult: isDifficult,
+      type: 'article',
+    },
   })
   return sentences
 }
-//
 
-export default async function Articles() {
-  const data = await getSentences()
+export default async function Articles({
+  searchParams,
+}: {
+  searchParams?: { [key: string]: string | string[] | undefined }
+}) {
+  const data = await getSentences(searchParams)
   return (
     <main className={styles.main}>
       <header>
-        <h2>Noun article practice</h2>
+        <h2>Article practice</h2>
         <ArticlesClient sentences={data} />
       </header>
     </main>
